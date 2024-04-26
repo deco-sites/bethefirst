f9bd2d2f96a6c2b1c1f310345a1ed800b47a3652
Clayton Severo <clayton.severo@rankme.com.br>
2024-04-26 11:37:55 -0300

    Desenvolvimento Projects

--- sections/Projects.tsx
+++ sections/Projects.tsx
@@ -1,37 +1,44 @@
 export interface Projects {
-   name: string;
-   image: {
-      src: string;
-      alt: string;
-   };
-   link: string
+   /**
+    * @title
+    */
+  name: string;
+  image: {
+    src: string;
+    alt: string;
+  };
+  link: string;
+  order: 'order-1' | 'order-2'
 }
 
 export interface Props {
-   title: string;
-   projects: Projects[];
+  title: string;
+  projects: Projects[];
 }
 
 export default function Projects({ title, projects }: Props) {
-   return (
-      <div className="bg-emerald-950 py-20">
-         <div className="container">
-            <h2 className="text-6xl text-white font-bold">{title}</h2>
-            <ul>
-               {projects.map((project, index) => (
-                  <li>
-                     <a href={project.link}>
-                        <div>
-                           <h3>{project.name}</h3>
-                        </div>
-                        <div>
-                           <img src={project.image.src} alt={project.image.alt} />
-                        </div>
-                     </a>
-                  </li>
-               ))}
-            </ul>
-         </div>
+  return (
+    <div className="bg-[#263424] py-20">
+      <div className="container">
+        <h2 className="text-8xl text-white mb-16">Latest <span className="block font-bold">Projects</span></h2>
+        <ul className="grid grid-cols-2 border border-slate-800">
+          {projects.map((project, index) => (
+            <li className="flex" key={index}>
+              <a href={project.link} className="grid grid-cols-2 w-full">
+                <div className={`p-4 ${project.order}`}>
+                  <h3 className="text-white">{project.name}</h3>
+                </div>
+                <div className={`${project.order === 'order-1' ? 'order-2' : 'order-1'} flex`}>
+                  <img src={project.image.src} alt={project.image.alt} className="w-full" />
+                </div>
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="flex justify-center mt-16">
+          <a href="#" className="btn btn-wide btn-circle">Contact Us</a>
+        </div>
       </div>
-   )
+    </div>
+  );
 }
--- sections/SpecialHero.tsx
+++ sections/SpecialHero.tsx
@@ -1,19 +1,19 @@
 export interface Props {
-   title: string;
-   text: string;
-   image: string;
+  title: string;
+  text: string;
+  image: string;
 }
 
 export default function SpecialHero({ title, text, image }: Props) {
-   return (
-      <div
-         className="bg-cover bg-no-repeat h-screen"
-         style={`background-image: url('${image}')`}
-      >
-         <div className="flex flex-col items-center">
-            <h1 className="text-8xl font-bold">{title}</h1>
-            <p className="text-4xl">{text}</p>
-         </div>
+  return (
+    <div
+      className="bg-cover bg-no-repeat h-screen"
+      style={`background-image: url('${image}')`}
+    >
+      <div className="flex flex-col items-center">
+        <h1 className="text-8xl font-bold">{title}</h1>
+        <p className="text-4xl">{text}</p>
       </div>
-   );
+    </div>
+  );
 }
