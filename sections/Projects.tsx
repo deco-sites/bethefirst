fdceb48de41d2a6dd77902a25c64131bbdb3447b
Clayton Severo <clayton.severo@rankme.com.br>
2024-04-26 11:38:19 -0300

    Desenvolvimento Projects

--- sections/Projects.tsx
+++ sections/Projects.tsx
@@ -1,14 +1,14 @@
 export interface Projects {
-   /**
-    * @title
-    */
+  /**
+   * @title
+   */
   name: string;
   image: {
     src: string;
     alt: string;
   };
   link: string;
-  order: 'order-1' | 'order-2'
+  order: "order-1" | "order-2";
 }
 
 export interface Props {
@@ -20,7 +20,9 @@ export default function Projects({ title, projects }: Props) {
   return (
     <div className="bg-[#263424] py-20">
       <div className="container">
-        <h2 className="text-8xl text-white mb-16">Latest <span className="block font-bold">Projects</span></h2>
+        <h2 className="text-8xl text-white mb-16">
+          Latest <span className="block font-bold">Projects</span>
+        </h2>
         <ul className="grid grid-cols-2 border border-slate-800">
           {projects.map((project, index) => (
             <li className="flex" key={index}>
@@ -28,8 +30,16 @@ export default function Projects({ title, projects }: Props) {
                 <div className={`p-4 ${project.order}`}>
                   <h3 className="text-white">{project.name}</h3>
                 </div>
-                <div className={`${project.order === 'order-1' ? 'order-2' : 'order-1'} flex`}>
-                  <img src={project.image.src} alt={project.image.alt} className="w-full" />
+                <div
+                  className={`${
+                    project.order === "order-1" ? "order-2" : "order-1"
+                  } flex`}
+                >
+                  <img
+                    src={project.image.src}
+                    alt={project.image.alt}
+                    className="w-full"
+                  />
                 </div>
               </a>
             </li>
